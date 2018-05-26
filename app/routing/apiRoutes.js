59cc2f4a66683b052d7df83506f9954bb09e0406
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-05-25 19:49:12 -0700

    Add to routes and JSON parse methods for post

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var friends = require('../data/friends');
 
 /* GET users listing. */
 router.get('/friends', function(req, res, next) {
-  res.send('send data file as json');
+  res.json(friends);
 });
 
 router.post('/friends', (req, res, next) => {
-  res.send('do file calc')
+  let { name, photo, ...answers } = req.body;
+  let answersArray = Object.values(answers);
+  const user = { name, photo, answersArray };
+  friends.push(user);
+  res.json(friends)
 });
 
 module.exports = router;
